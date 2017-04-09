acd60f7c15ad43f01f5bcb65e5973a3ef257783a
JoaoVeloso <7140201@my.ipleiria.pt>
2017-04-09 18:59:54 +0100

    Backup
    Backup

--- Horario.java
+++ Horario.java
@@ -1,42 +1,38 @@
-/**
- * Created by Joao on 06/04/2017.
- */
 public class Horario {
+    int duracao;
     DiaDaSemana diaDaSemana;
     int hora;
     int minutos;
-    int duracao;
-
-    public void validacoes(){
-        validarDuracao(duracao);
-        validarHora(hora);
-        validarMinutos(minutos);
-    }
 
     private int validarHora(int hora){
-        if (hora > 23 || hora < 0)
-            System.out.println("A Hora não está compreendida entre as 23 e as 0.");
+        if (hora > 23 || hora < 0) {
+            System.out.println("A hora não está compreendida entre 0 e 23");
+            hora = 2;
+        }
+        System.out.println(hora);
         return hora;
     }
 
     private int validarMinutos(int minutos){
-        if (minutos > 59 || minutos < 00)
-            System.out.println("Os minutos não estão compreendidos entre 00 e 59.");
+        if (minutos > 59 || minutos < 0)
+            System.out.println("Os minutos não estão compreendidos entre 0 e 59");
         return minutos;
     }
 
     private int validarDuracao(int duracao){
-        if (duracao > 180 || duracao < 0)
-            System.out.println("A duração não está compreendida entre 0 e 180.");
+        if(duracao > 180 || duracao < 0)
+            System.out.println("A Duracao tem que estar compreendida entre 0 a 180 minutos");
         return duracao;
     }
 
+
     public Horario(DiaDaSemana diaDaSemana, int hora, int minutos, int duracao) {
         this.diaDaSemana = diaDaSemana;
-        this.hora = hora;
-        this.minutos = minutos;
-        this.duracao = duracao;
+        this.hora = validarHora(hora);
+        this.minutos = validarMinutos(minutos);
+        this.duracao = validarDuracao(duracao);
     }
+
 
 
 }
--- Main.java
+++ Main.java
@@ -1,19 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Joao on 01/04/2017.
- */
 public class Main {
-    public static final void main(String[] args){
-        Professor professor = new Professor(2161797, "João Veloso");
+    public static void main(String[] args){
+        Professor professor = new Professor(2161174, "João Castelo");
         List<Aluno> alunos = new ArrayList<>();
 
-        Aula aula = new Aula(1, "P2", professor);
-        Horario horario = new Horario(DiaDaSemana.TERÇA,2,30,90);
-        horario.validacoes();
-        professor.adicionar(aula);
+        Aula aula = new Aula(3, "P2", professor);
 
+        Horario horario = new Horario(DiaDaSemana.SEGUNDA, -10, 3, 3);
+
+        professor.adicionar(aula);
         professor.assinarSumario(aula);
 
         System.out.println(aula.getSumario());
